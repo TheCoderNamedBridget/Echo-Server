ed02b72c251338de7d49959c0963e783cff70789
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-09-30 14:03:44 -0700

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -9,23 +9,18 @@ import java.io.*;
 public class EchoServer {
 
 	public static void main(String[] args) {
-		System.out.println("Server started");
+		//System.out.println("Server started");
 		try {
 			// started lab at 6013 changed to 17
 			ServerSocket sock = new ServerSocket(6017);
 			Boolean checkSocket = true;
 			Socket client = sock.accept();
+			
 			/* now listen for connections */
 			while (checkSocket) {
-				System.out.println("in loop");
-				
-				System.out.println("in loop here");
 				InputStream in = client.getInputStream();
-				
-				System.out.println("in loop2");
 				PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-				System.out.println("in loop3");
-				System.out.println("in = " + in.read());
+				
 				if ( in.read() != -1)
 				{
 					//System.out.println("byte data " + in.read());
@@ -39,11 +34,11 @@ public class EchoServer {
 					//take input from server and return correct output
 					returnMsg = returnMsg.replaceAll("client", "server");
 					/* Write the date to the socket */
-					pout.println("return string: " + returnMsg);
-					System.out.println("return string: " + returnMsg);
+					pout.println( returnMsg);
+					System.out.println( returnMsg);
 					/* close the socket and resume */
 					/* resume listening for connections */
-					client.close();
+					//client.close();
 					// sock.close();
 				}
 				else
@@ -58,6 +53,6 @@ public class EchoServer {
 		} catch (IOException ioe) {
 			System.err.println(ioe);
 		}
-		System.out.println("exit while loop");
+		//System.out.println("exit while loop");
 	}
 }
