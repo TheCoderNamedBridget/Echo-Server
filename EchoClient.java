e3a7910966fcf36eee94833de8f1975e71057959
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-09-30 14:03:58 -0700

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -12,14 +12,14 @@ public class EchoClient {
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Client started");
+		//System.out.println("Client started");
 		try {
 			/*Make connection to server socket*/
 			Socket sock = new Socket("127.0.0.1", 6017);
 			
 			PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
-			pout.println(1111);
-			System.out.println(1111);
+			pout.println("hello I am the client. I was busy doing client things yesterday when my client doctor called. he says I have client itis");
+			System.out.println("hello I am the client. I was busy doing client things yesterday when my client doctor called. he says I have client itis");
 			/*close the socket connection*/
 			sock.close();
 //			pout.close();
